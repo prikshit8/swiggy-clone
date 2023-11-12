fcbd09eab5d71c536191fc4a38ce77cfda9ac0c1
prikshit <prikshitchawla.v@gmail.com>
2023-11-12 18:11:19 +0530

    1 - inception

---
+++ app.js
@@ -0,0 +1,32 @@
+const parent = React.createElement(
+  "div",
+  {
+    id: "parent",
+  },
+  [
+    React.createElement(
+      "h1",
+      {
+        id: "heading",
+      },
+      "Hello world from react"
+    ),
+    React.createElement(
+      "h2",
+      {
+        id: "heading",
+      },
+      "Hello world from react h2"
+    ),
+  ]
+);
+
+// const heading = React.createElement(
+//   "h1",
+//   {
+//     id: "heading",
+//   },
+//   "Hello world from react"
+// );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
