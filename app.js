6b6919cab95a8fbda4d93ac60325c10189671ee9
prikshit <prikshitchawla.v@gmail.com>
2023-11-13 10:11:14 +0530

    3 - laying the foundation - part - 1

--- app.js
+++ app.js
@@ -1,35 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  {
-    id: "parent",
-  },
-  [
-    React.createElement(
-      "h1",
-      {
-        id: "heading",
-      },
-      "Hello world from react"
-    ),
-    React.createElement(
-      "h2",
-      {
-        id: "heading",
-      },
-      "Hello world from react h2"
-    ),
-  ]
-);
+// React.createElement => ReactElement - JS Object => HTMLElement(render)
 
-// const heading = React.createElement(
-//   "h1",
-//   {
-//     id: "heading",
-//   },
-//   "Hello world from react"
-// );
+const heading = React.createElement("h1", { id: "heading" }, "namaste react");
+
+// JSX is not HTML in js
+// it's html-like or XML-like syntax & it's not pure js
+// and browser will not understand it
+// jsxHeading is equivalent to heading
+// JSX is transpiled into React object before it reaches the JS Engine - it's done by PARCEL
+// using Babel (it's a JS Package installed with PARCEL)
+
+// JSX => React.createElement => ReactElement - JS Object => HTMLElement(render)
+const jsxHeading = <h1 id="heading">namaste react</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(heading);
