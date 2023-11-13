3d82f2bcb0ea8797503c718da2ccab29e877f9d7
prikshit <prikshitchawla.v@gmail.com>
2023-11-13 11:43:58 +0530

    3 - laying the foundation - part - 2

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement => ReactElement - JS Object => HTMLElement(render)
-
-const heading = React.createElement("h1", { id: "heading" }, "namaste react");
-
-// JSX is not HTML in js
-// it's html-like or XML-like syntax & it's not pure js
-// and browser will not understand it
-// jsxHeading is equivalent to heading
-// JSX is transpiled into React object before it reaches the JS Engine - it's done by PARCEL
-// using Babel (it's a JS Package installed with PARCEL)
-
-// JSX => React.createElement => ReactElement - JS Object => HTMLElement(render)
-const jsxHeading = <h1 id="heading">namaste react</h1>;
+const Title = () => <h1 className="head">Namaste JSX</h1>;
+const number = 10000;
+// React Functional Components
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <Title /> {number}
+      <Title></Title>
+      {Title()}
+      {/* component composition, using component inside other component*/}
+      {/* {write any JS inside {} braces} */}
+      <h1 className="heading">Namaste React functional Component</h1>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent />);
